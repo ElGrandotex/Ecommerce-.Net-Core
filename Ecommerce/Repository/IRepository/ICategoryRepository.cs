9ff653565fe1a69e2229e0524d0e2ae48b938e08
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-09-18 00:17:28 -0500

    Añadida implementación de repositorio de categorías

---
+++ Ecommerce/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Model;
+
+namespace Ecommerce.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int categoryId);
+        bool CategoryExists(int categoryId);
+        bool CategoryExists(string name);
+        bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
+
+    }
+}
