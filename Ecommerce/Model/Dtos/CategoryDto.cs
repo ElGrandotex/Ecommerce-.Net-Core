d132903bad3fedc7de0ef76ecf74e5aacbdeb4ac
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-09-18 00:18:25 -0500

    DTOs

---
+++ Ecommerce/Model/Dtos/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Model.Dtos
+{
+    public class CategoryDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ Ecommerce/Model/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Model.Dtos
+{
+    public class CreateCategoryDto
+    {
+        //No va porque este se genera en la base de datos
+        //public int ID { get; set; }
+
+        [Required(ErrorMessage = "Nombre Obligatorio")]
+        [MaxLength(50, ErrorMessage = "El nombre no puede tener mas de 50 caracteres")]
+        [MinLength(3, ErrorMessage = "El nombre no puede tener menos de 3 caracteres")]
+        public string Name { get; set; } = string.Empty;
+        
+        //se asgina cuando se utiliza este dto
+        //public DateTime CreationDate { get; set; }
+    }
+}
